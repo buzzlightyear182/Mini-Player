fa5fef48c3e80cbf28cf9564abb0df92d97b0af5
Ricard Sole <ricard.solecasas@gmail.com>
2014-07-13 20:16:46 +0200

    changed the updateList method to remove the empty class

--- metadata.js
+++ metadata.js
@@ -48,44 +48,72 @@
   });
 
   document.getElementById('audio').addEventListener('timeupdate', function(event) {
-    var audio = document.getElementById('audio');
-    document.querySelector('.seekbar progress').value = audio.currentTime;
+    document.querySelector('.seekbar progress').value = this.currentTime;
   });
 
-  // document.querySelector('form').addEventListener('submit', function (event) {
-  //   event.preventDefault();
-  //   var track = event.target[0].value.split(':')[2];
-  //   fetchMetaData(track);
-  // });
+  document.getElementById('audio').addEventListener('ended', function(event) {
+    document.querySelector('.seekbar progress').value = 0;
+    document.querySelector('.btn-play').classList.remove('playing');
+  });
 
-  // var fetchMetaData = function(track){
-  //   var info = new XMLHttpRequest();
-  //   var baseURL = "https://api.spotify.com/v1/tracks/";
-  //   info.open('GET', baseURL + track);
-
-  //   info.setRequestHeader('Accept', 'application/json');
-
-  //   info.onload = function() {
-  //     if (this.status === 200) {
-  //       var response = JSON.parse(this.response);
-  //       displayInfo(response);
-  //     }
-  //   };
-
-  //   info.send();
-  // };
+  document.querySelector('#results').addEventListener('click', function(event) {
+    event.preventDefault();
+    if (event.target.nodeName === 'A') {
+      fetchMetaData(event.target.href.split(':')[2]);
+    } else if (event.target.nodeName === 'SPAN') {
+      fetchMetaData(event.target.parentElement.href.split(':')[2]);
+    }
+  });
 
   document.getElementById('Search').addEventListener('submit', function(event) {
     event.preventDefault();
     var query = event.target[0].value;
     searchItem(query);
-  }
-  );
+  });
 
-  var searchItem = function(query){
+  var concatArtists = function(a) {
+    return a.length === 1 ? a[0].name : a.reduce(function(previous, next) {
+      if (previous.name) {
+        return previous.name + ', ' + next.name;
+      }
+
+      return previous + ', ' + next.name;
+    });
+  };
+
+  var updateList = function(songs) {
+    var element = document.getElementById('results');
+    element.innerHTML = "";
+
+    for (var i = 0; i < songs.length; i++) {
+      var el = document.createElement('li');
+      var anchor = document.createElement('a');
+      var artist = document.createElement('span');
+      var song = document.createElement('span');
+      var cover = document.createElement('img');
+
+      var artist_text = concatArtists(songs[i].artists);
+
+      artist.classList.add('artist');
+      artist.innerHTML = artist_text;
+
+      song.classList.add('song');
+      song.innerHTML = songs[i].name;
+
+      anchor.appendChild(artist);
+      anchor.appendChild(song);
+      anchor.href = songs[i].uri;
+      el.appendChild(anchor);
+      element.appendChild(el);
+    }
+
+    element.classList.remove('empty');
+  };
+
+  var searchItem = function(query) {
     var searchRequest = new XMLHttpRequest();
     var base_url = "https://api.spotify.com/v1/search?q=";
-    var searchURL = base_url + query + "&type=track&limit=5";
+    var searchURL = base_url + encodeURIComponent(query) + "&type=track&limit=3";
     searchRequest.open('GET', searchURL);
 
     searchRequest.setRequestHeader('Accept','application/json');
@@ -93,7 +121,7 @@
     searchRequest.onload = function () {
       if (this.status === 200) {
         var results = JSON.parse(this.response);
-        console.log(results);
+        updateList(results.tracks.items);
       }
     };
 
